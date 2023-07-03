1a937be9f79e73437afd1ca8ccd262268a958ad7
hongjunland <zunza96@gmail.com>
2023-07-03 20:21:45 +0900

    feat: init

---
+++ src/main/java/com/tddnote/TddNoteApplication.java
@@ -0,0 +1,13 @@
+package com.tddnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tddnote/TddNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tddnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
